b29b6851aeed4ad19b9f51ef475c0747be86a814
Dc471 <Dc@gmail.com>
2025-04-08 19:07:15 -0500

    Primer algoritmo

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -10,6 +10,17 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            int num1;
+            int num2;
+            int suma;
+            Console.Write("Ingrese numero 1: ");
+            num1=int.Parse(Console.ReadLine());
+            Console.Write("Ingrese numero 2: ");
+            num2=int.Parse(Console.ReadLine());
+            suma = num1 + num2;
+            Console.WriteLine("La suma es: "+suma);
+            
+            //Console.ReadKey();
         }
     }
 }
